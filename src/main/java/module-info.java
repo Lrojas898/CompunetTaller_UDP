9b26d08bedd3245c0a73ad5e937066a5f1fc218f
Oscar Munoz Ramirez <1083870123@u.icesi.edu.co>
2024-09-04 20:45:49 -0500

    The development environment of the UDP conection activity has been created.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.compunettaller_udp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.compunettaller_udp to javafx.fxml;
+    exports com.example.compunettaller_udp;
+}
